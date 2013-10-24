98bfe579305a0909d71d1ceb409a76c08095d574
Jesper Jalvemo <jesper.jalvemo@videoplaza.com>
2013-10-24 17:37:02 +0200

    updated testbed with api documentation and removed name from register form

--- testBot.js
+++ testBot.js
@@ -1,56 +1,154 @@
+/**
+
+server UI:  (to prevent image flickering run in firefox)
+    http://staff.videoplaza.org:8081/
+    press new game
+    register your bots (the game id is in the url path)
+
+to register a bot go a game to:
+    http://staff.videoplaza.org:8081/register.html
+
+
+bot API:
+your bot need to respond to the following Json post calls:
+    on "http://yourIp:yourPort/start" you should return yor bot details in json
+        {
+            name : "Your bot name",
+            version : "0.1",
+            avatar : "http://yourBotImageUrl30x30px.png"
+        }
+
+    on "http://yourIp:yourPort/move" you should return yor move in json
+        {
+            direction: {x:1,y:0}, // possible directions: {x:1,y:0} || {x:-1,y:0} || {x:0,y:1} || {x:0,y:-1}
+            mine: layMine  // 1 || 0
+        }
+
+
+ the json posted to your bot will contain the game state
+ the most important parts you need use are:
+     board
+        2d array with the mines/bombs, bots and gold.
+        possible values:
+            'g' = gold
+            'e' = empty
+            'b' = bomb
+            [integer] = bot id. The id is also the bots index in the bots array
+
+      yourID
+        your bot id. You can access your bot position on the board by: bots[yourID].position
+
+     settings.botTimeout
+        make sure your bot responds fast. if it doesn't, it will die!
+
+ this is the json object posted to your bot:
+    {
+        "id": 0,
+        "round": 60, // game ends when round hits settings.gameLength
+        "bots": [
+            {
+                "id": 0,
+                "name": "Jesper testBot",
+                "version": "0.1",
+                "avatar": "http://a.deviantart.net/avatars/d/r/dr-robot.png?1",
+                "host": "127.0.0.1",
+                "port": 1337,
+                "position": {
+                    "x": 5,
+                    "y": 7
+                },
+                "points": 0,
+                "alive": true
+            },
+            {
+                "id": 1,
+                "name": "Jesper testBot",
+                "version": "0.1",
+                "avatar": "http://a.deviantart.net/avatars/d/r/dr-robot.png?1",
+                "host": "127.0.0.1",
+                "port": 1337,
+                "position": {
+                    "x": 7,
+                    "y": 7
+                },
+                "points": 0,
+                "alive": true
+            }],
+        "timer": 440, // game ends on 0
+        "board": [
+            ["e","e","e","e","e","e","g","e","e","e"],
+            ["b","g","g","e","e","e","e","e","e","e"],
+            ["e","b","e","e","e","e","e","e","e","e"],
+            ["b","e","b","b","b","e","e","e","e","e"],
+            ["b","e","b","b","e","b","b","e","e","e"],
+            ["e","e","b","e","e","e","e", 0 ,"e","e"],
+            ["b","e","e","e","e","e","e","g","e","e"],
+            ["e","e","g","e","e","e","e", 1 ,"e","e"],
+            ["b","e","e","e","e","e","b","b","e","e"],
+            ["b","e","e","e","e","e","b","b","e","e"],
+            ["e","e","e","e","e","e","e","e","e","e"]],
+         "settings":{
+                "turnDuration": 200,
+                "botTimeout": 2000, // if the game server don't have the response in 2000ms yor bot will die
+                "gameLength": 500, // maximum turns before the game ends
+                "boardSize": { "x": 15, "y": 20},
+                "goldFrequency": 5 // how often gold spawn
+         },
+        "boardUpdates": [],
+        "yourID": 1
+    }
+
+ */
 
 var http = require('http');
-
 var port = 1337;
-var serverHost = "127.0.0.1";
-var players = 15;
+
+http.createServer(function (request, res) {
+    // handle the routes
+    if (request.method == 'POST') {
 
 
-for (var i = 0; i < players; i++) {
-    http.createServer(function (request, res) {
-        // handle the routes
-        if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
 
-
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-
-                console.log("message received.");
-                console.log(request.url);
+            console.log("message received.");
+            console.log(request.url);
 //                console.log(body);
-                body = JSON.parse(body);
+            body = JSON.parse(body);
 
 
-                var response = {};
+            var response = {};
 
-                if (request.url == '/ping'){
-                    response = onPing();
-                }
-                else if (request.url == '/start'){
-                    response = onStart(body);
-                }
-                else if (request.url == '/move'){
+            if (request.url == '/ping'){
+                response = onPing();
+            }
+            else if (request.url == '/start'){
+                response = onStart(body);
+            }
+            else if (request.url == '/move'){
 
-                    response = onMove(body);
-                }
-                else if (request.url == '/log'){
-                    response = onLog(body);
-                }
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(JSON.stringify(response));
-                res.end();
+                response = onMove(body);
+            }
+            else if (request.url == '/log'){
+                response = onLog(body);
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(response));
+            res.end();
 
-            });
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Hello THis is Tha BOT.\n');
-        }
-    }).listen(port + i, '0.0.0.0');
-}
+        });
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello THis is Tha BOT.\n');
+    }
+}).listen(port, '0.0.0.0');
+//}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
 function onPing(){
     return {message: "pong"};
 }
@@ -106,6 +204,3 @@ function onLog(data){
     return {};
 }
 
-
-//}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
