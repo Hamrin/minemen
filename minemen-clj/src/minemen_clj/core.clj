44e97a45eea3585af773c8f7a1d364924ff69dfc
joshua <josh@videoplaza.com>
2013-10-25 17:21:15 +0200

    add find-mines

--- minemen-clj/src/minemen_clj/core.clj
+++ minemen-clj/src/minemen_clj/core.clj
@@ -1,18 +1,52 @@
 (ns minemen-clj.core
   (:require [clojure.data.json :as json]))
 
+(defn indexed-board
+  "returns the board as a map where keys are the row indexes and values are the rows"
+  [state]
+  (zipmap (range) (state "board")))
+
 (defn loc
   "returns this bot's location on the board as [y x]"
   [state]
-  (let [id (state "yourID")
-        iboard (zipmap (range) (state "board"))]
-    (first (filter #(> (last %) -1) (map #(vector (first %) (.indexOf (last %) id)) iboard)))))
+  (let [id (state "yourID")]
+    (first (filter #(> (last %) -1) (map #(vector (first %) (.indexOf (last %) id)) (indexed-board state))))))
 
 (defn size
   "returns size of board as [y x]"
   [state]
   (let [board (state "board")]
     [(count board) (count (nth board 1))]))
+
+(defn seq-contains?
+  "tests if value v exists in seq s"
+  [v s]
+  (some #{v} s))
+
+(defn indexes-of
+  "returns indexes in seq s at which value v exists"
+  [v s]
+  (map first (filter #(= v (last %)) (zipmap (range) s))))
+
+(defn find-in-row
+  "returns the locations of value v in in row i as a vector of [y x] positions"
+  [i v row]
+  (map #(vector i %) (indexes-of v row)))
+
+(defn find-on-board
+  "returns the locations of value v as a vector of [y x] positions"
+  [state v]
+  (mapcat #(find-in-row (first %) v (last %)) (indexed-board state)))
+
+(defn find-gold
+  "returns the locations of gold as a vector of [y x] positions"
+  [state]
+  (find-on-board state "g"))
+
+(defn find-mines
+  "returns the location of mines as a vector of [y x] positions"
+  [state]
+  (find-on-board state "b"))
 
 (defn move [board]
   (println board)
--- minemen-clj/test/minemen_clj/test/core.clj
+++ minemen-clj/test/minemen_clj/test/core.clj
@@ -10,4 +10,24 @@
     (is (= [7 7] (loc state))))
 
   (testing "size of board"
-    (is (= [11 10] (size state)))))
+    (is (= [11 10] (size state))))
+
+  (testing "find gold"
+    (let [gold (find-gold state)]
+      (is (= 5 (count gold)))
+      (is (seq-contains? [0 6] gold))
+      (is (seq-contains? [1 1] gold))
+      (is (seq-contains? [1 2] gold))
+      (is (seq-contains? [6 7] gold))
+      (is (seq-contains? [7 2] gold))))
+
+  (testing "find mines"
+    (let [mines (find-mines state)]
+      (is (= 19 (count mines)))
+      (is (seq-contains? [1 0] mines))
+      (is (seq-contains? [3 0] mines))
+      (is (seq-contains? [3 2] mines))
+      (is (seq-contains? [3 3] mines))
+      (is (seq-contains? [3 4] mines))
+      (is (seq-contains? [5 2] mines))
+      (is (seq-contains? [9 6] mines)))))
